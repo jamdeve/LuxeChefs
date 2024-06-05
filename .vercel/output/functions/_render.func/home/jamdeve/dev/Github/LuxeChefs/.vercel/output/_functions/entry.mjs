6fbd40cffbd6461ee2aa99b1ba982ec7a890cbc5
Jamdeve <jujuturu742@gmail.com>
2024-06-05 18:37:26 +0200

    a

--- .vercel/output/functions/_render.func/home/jamdeve/dev/Github/LuxeChefs/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/home/jamdeve/dev/Github/LuxeChefs/.vercel/output/_functions/entry.mjs
@@ -1,5 +1,5 @@
 import { renderers } from './renderers.mjs';
-import { l as levels, g as getEventPrefix, L as Logger, A as AstroIntegrationLogger, manifest } from './manifest_DQPtZE_l.mjs';
+import { l as levels, g as getEventPrefix, L as Logger, A as AstroIntegrationLogger, manifest } from './manifest_BFM4h6UC.mjs';
 import { e as appendForwardSlash, j as joinPaths, t as trimSlashes, f as fileExtension, s as slash, p as prependForwardSlash, g as removeTrailingForwardSlash, h as collapseDuplicateSlashes } from './chunks/astro/assets-service_DTGUZxl8.mjs';
 import { R as ROUTE_TYPE_HEADER, l as REROUTE_DIRECTIVE_HEADER, A as AstroError, n as ResponseSentError, o as MiddlewareNoDataOrNextCalled, p as MiddlewareNotAResponse, G as GetStaticPathsRequired, q as InvalidGetStaticPathsReturn, t as InvalidGetStaticPathsEntry, u as GetStaticPathsExpectedParams, v as GetStaticPathsInvalidRouteParam, P as PageNumberParamNotFound, D as DEFAULT_404_COMPONENT, N as NoMatchingStaticPathFound, w as PrerenderDynamicEndpointPathCollide, x as ReservedSlotName, y as renderSlotToString, z as renderJSX, B as chunkToString, C as isRenderInstruction, H as LocalsNotAnObject, J as clientLocalsSymbol, K as clientAddressSymbol$1, O as PrerenderClientAddressNotAvailable, Q as ClientAddressNotAvailable, S as StaticClientAddressNotAvailable, T as renderPage, V as renderEndpoint, W as ASTRO_VERSION, X as AstroResponseHeadersReassigned, Y as responseSentSymbol$1, Z as REROUTABLE_STATUS_CODES } from './chunks/astro_DcP4r1Hm.mjs';
 import { serialize, parse } from 'cookie';
@@ -2345,7 +2345,7 @@ const _manifest = Object.assign(manifest, {
     middleware: onRequest
 });
 const _args = {
-    "middlewareSecret": "1b14331f-6b9f-4e59-9164-732f56c4ac41",
+    "middlewareSecret": "35774c5c-cefd-4d4b-addc-07ed4431cb99",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
