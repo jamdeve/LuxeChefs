290f49d385a4f88eb648903c16521ea43f83b4b9
Jamdeve <jujuturu742@gmail.com>
2024-06-27 12:35:52 +0200

    fix

--- .netlify/functions-internal/ssr/chunks/generic_DZE-ZSUu.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_Bx_QBk6V.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/trabajar_BexqhkkY.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/trabajar_Bl2qPg8B.mjs');
-
-export { page };
